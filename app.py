4d497207b6aa945c28cc8d3b858c0e831b9e4355
epithap <upiwae6@gmail.com>
2026-09-21 19:34:53 +0700

    sukirebus

--- app.py
+++ app.py
@@ -24,10 +24,9 @@ def home():
 @app.route('/s3-data')
 def get_s3_data():
     try:
-        # Mengambil file teks dari Amazon S3 Bucket
         s3 = boto3.client('s3', region_name='us-east-1')
         bucket_name = 'ks-storage-app-epithap'
-        file_key = 'pesan.txt.txt'  # Sesuaikan dengan nama file yang ada di S3-mu
+        file_key = 'pesan.txt.txt'
         
         response = s3.get_object(Bucket=bucket_name, Key=file_key)
         content = response['Body'].read().decode('utf-8')
@@ -35,6 +34,6 @@ def get_s3_data():
         return jsonify({"status": "sukses", "pesan_dari_s3": content}), 200
     except Exception as e:
         return jsonify({"status": "gagal", "pesan": str(e)}), 500
-
+        
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
